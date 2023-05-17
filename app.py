f75a6f47fc3de429d0fe69ee970bc431d64b7c6b
Jun Young <50202150+wsdx123@users.noreply.github.com>
2023-05-17 19:27:05 +0900

    fix: 화면 디자인

--- app.py
+++ app.py
@@ -5,11 +5,9 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://sparta:test@cluster0.wrgvto4.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
-
 @app.route('/')
 def home():
-   return render_template('index.html')
-
+    return render_template('index.html')
 @app.route("/comment", methods=["POST"])
 def guestbook_post():
     name_receive = request.form['name_give']
@@ -26,6 +24,8 @@ def guestbook_post():
 def guestbook_get():
     all_comments = list(db.eight.find({},{'_id':False}))
     return jsonify({'result': all_comments})
-
+@app.route('/team1')
+def team1():
+    return render_template('team1.html')
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
