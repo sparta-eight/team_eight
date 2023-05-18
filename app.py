2e9f4d06da6b6271e941e6962d771c236ea8d30a
Jun Young <50202150+wsdx123@users.noreply.github.com>
2023-05-18 21:05:44 +0900

    last commit

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ db = client.dbsparta
 @app.route('/')
 def home():
     return render_template('index.html')
+
 @app.route("/comment", methods=["POST"])
 def guestbook_post():
     name_receive = request.form['name_give']
@@ -20,12 +21,32 @@ def guestbook_post():
 
     return jsonify({'msg': '저장 완료!'})
 
+@app.route("/comment/delete", methods=["POST"])
+def guestbook_delete():
+    name_receive = request.form['name_give']
+    db.eight.delete_one({'name':name_receive})
+    return jsonify({'msg': '삭제 완료!'})
+
 @app.route("/comment", methods=["GET"])
 def guestbook_get():
     all_comments = list(db.eight.find({},{'_id':False}))
     return jsonify({'result': all_comments})
+
 @app.route('/team1')
 def team1():
     return render_template('team1.html')
+
+@app.route('/team2')
+def team2():
+    return render_template('team2.html')
+
+@app.route('/team3')
+def team3():
+    return render_template('team3.html')
+
+@app.route('/team4')
+def team4():
+    return render_template('team4.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
